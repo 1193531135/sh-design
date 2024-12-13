19d873ae2d4f35959340683dc3c28a35951940d3
1193531135 <1193531135@qq.com>
2024-12-13 18:50:29 +0800

    新增list

---
+++ src/components/pages/dataBase/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          title: "1",
+          content: "1"
+        },
+        {
+          title: "2",
+          content: "2"
+        },
+        {
+          title: "3",
+          content: "3"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
