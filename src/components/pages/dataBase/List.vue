813b2dde0554018b206433b42eac07caf1977d8f
1193531135 <1193531135@qq.com>
2024-12-17 15:57:42 +0800

    新增heart状态，和noData状态

--- src/components/pages/dataBase/List.vue
+++ src/components/pages/dataBase/List.vue
@@ -31,18 +31,26 @@
       </div>
     </div>
     <div class="list-container">
-      <div class="card-grid" v-infinite-scroll="loadData">
+      <div class="card-grid" v-show="listData.length != 0" v-infinite-scroll="loadData">
         <!-- 联系客服 -->
         <div class="chart">
-          <img src="../../../assets/image/home/chart.png" alt="">
+          <img src="../../../assets/image/dataBase/chart.png" alt="">
           <div>联系客服</div>
         </div>
-        <el-image v-for="(itemData, index) in listData.filter((i, index) => index < randerIndex)" :key="index"
-          class="card-item" :ref="`card-${index}`" @load="imageLoaded(itemData, `card-${index}`)"
-          @error="imageLoaded(itemData, `card-${index}`)" :src="itemData.url">
-        </el-image>
+        <div v-for="(itemData, index) in listData.filter((i, index) => index < randerIndex)" :key="index"
+          :ref="`card-${index}`" class="card-item">
+          <el-image @load="imageLoaded(itemData, `card-${index}`)" @error="imageLoaded(itemData, `card-${index}`)"
+            :src="itemData.url">
+          </el-image>
+          <div class="heart" @click="heartClick(itemData)">
+            <img v-show="itemData.heart === false" src="../../../assets/image/dataBase/heart.png">
+            <img v-show="itemData.heart === true" src="../../../assets/image/dataBase/heart-red.png">
+          </div>
+        </div>
       </div>
-      <div v-if="listData.length === 0">没有更多了</div>
+      <div class="no-data" v-show="listData.length === 0">
+        <img src="../../../assets/image/dataBase/noData.png" alt="">
+      </div>
     </div>
   </div>
 </template>
@@ -126,7 +134,7 @@ export default {
     // 列表图片load回调布局
     imageLoaded(itemData, refName) {
       itemData.load = false
-      const el = this.$refs[refName][0].$el
+      const el = this.$refs[refName][0]
       // 重新布局
       this.$nextTick(() => {
         const rows = Math.ceil(el.clientHeight / 5) + 2
@@ -137,7 +145,7 @@ export default {
     reloadLayout() {
       this.$nextTick(() => {
         this.listData.filter((i, index) => index < this.randerIndex).forEach((itemData, index) => {
-          const el = this.$refs[`card-${index}`][0].$el
+          const el = this.$refs[`card-${index}`][0]
           const rows = Math.ceil(el.clientHeight / 5) + 2
           el.style.gridRowEnd = `span ${rows}`
         })
@@ -158,32 +166,39 @@ export default {
           data: {
             code: 200,
             data: [
-              { url: "https://th.bing.com/th/id/OIP.AnfpGRH9jn5ISlr-MRh7PwHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.AnfpGRH9jn5ISlr-MRh7PwHaHa?123" },
-              { url: "https://th.bing.com/th/id/OIP.Sd5fZmx5V064n2B4v5kVlgHaFT" },
-              { url: "https://th.bing.com/th/id/OIP.aacN2mrGOPxMdnO9RG9NJwHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.Kum8his5jIIH0U4zlV46GQHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.ccAGfkHGtJ4CNqD7AD_AbgHaHQ" },
-              { url: "https://th.bing.com/th/id/OIP.NukYPnWHTwNxgfn5zp22TgHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.Vx8OfvbfX4ZEpFxP05vj_gHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.wm-O33o4GamI5C3O4GJpIQHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.YFIYgExQTg-yGtiVp2es8gHaIO" },
-              { url: "https://th.bing.com/th/id/OIP.sM12wu1cQlq-7_6nTSx0ywHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.3AbuIshgNRobf190FR6vTQHaFj" },
-              { url: "https://th.bing.com/th/id/OIP.QBJ8DQgk6iLB-vKWVDtRYAHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.GuGKa8bmMqkhqbJLKGNYywHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.682w5icroBT76KIvze665AHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.kX5rlFiVwkleetaz2LX2MgHaJQ" },
-              { url: "https://th.bing.com/th/id/OIP.H_glcHLe6pj0NyLY_cdjvgHaHL" },
-              { url: "https://th.bing.com/th/id/OIP.idn8yhl_41JWeeelsIInFwHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.R2IOeYTxw_0pwC0qX7tm0AHaHa" },
-              { url: "https://th.bing.com/th/id/OIP.BaSc9hL_BLgkhnauyLj0zwHaGl" },
+              { url: "https://th.bing.com/th/id/OIP.AnfpGRH9jn5ISlr-MRh7PwHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.AnfpGRH9jn5ISlr-MRh7PwHaHa?123", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.Sd5fZmx5V064n2B4v5kVlgHaFT", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.aacN2mrGOPxMdnO9RG9NJwHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.Kum8his5jIIH0U4zlV46GQHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.ccAGfkHGtJ4CNqD7AD_AbgHaHQ", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.NukYPnWHTwNxgfn5zp22TgHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.Vx8OfvbfX4ZEpFxP05vj_gHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.wm-O33o4GamI5C3O4GJpIQHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.YFIYgExQTg-yGtiVp2es8gHaIO", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.sM12wu1cQlq-7_6nTSx0ywHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.3AbuIshgNRobf190FR6vTQHaFj", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.QBJ8DQgk6iLB-vKWVDtRYAHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.GuGKa8bmMqkhqbJLKGNYywHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.682w5icroBT76KIvze665AHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.kX5rlFiVwkleetaz2LX2MgHaJQ", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.H_glcHLe6pj0NyLY_cdjvgHaHL", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.idn8yhl_41JWeeelsIInFwHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.R2IOeYTxw_0pwC0qX7tm0AHaHa", heart: false },
+              { url: "https://th.bing.com/th/id/OIP.BaSc9hL_BLgkhnauyLj0zwHaGl", heart: false },
             ],
             message: "SUCCESS"
           }
         }
-        // 模拟数据
-        let randerListData = responese.data.data.map(i => ({ url: i.url + '?' + parseInt(Math.random() * 100) }))
+        let randerListData = responese.data.data
+        // 模拟数据代码块，和responese一起删除
+        {
+          // 模拟数据 复制多个假数据
+          responese.data.data.some(i => { i.url = i.url + '?' + parseInt(Math.random() * 100) })
+          for (let i = 0; i < 4; i++) {
+            randerListData.push(...randerListData)
+          }
+        }
         // 初始化load 状态
         const img = document.createElement('img')
         randerListData.some(i => {
@@ -191,12 +206,9 @@ export default {
           img.src = i.url
           i.load = !img.complete
         })
-        // 复制多个假数据
-        for (let i = 0; i < 4; i++) {
-          randerListData.push(...randerListData)
-        }
-        this.listData = randerListData
-        // 初始条数
+        // this.listData = randerListData
+        this.listData = []
+        // 初始展示条数
         this.randerIndex = 20
         // 首次布局
         this.reloadLayout()
@@ -208,7 +220,29 @@ export default {
     // 获取分类配置
     getTypeOptions() { },
     // 获取排序配置
-    getSortOptions() { }
+    getSortOptions() { },
+    // 设置like
+    async heartClick(cardData) {
+      let goalState = !cardData.heart
+      // 先进行页面的状态切换
+      cardData.heart = goalState
+      // let rep = await new Promise((re) => {
+      //   // goalState ? like():doLike()
+      //   goalState ? 
+      //   setTimeout((res) => {
+      //     // 执行like方法
+      //     re(res)
+      //   }):
+      //   setTimeout((res) => {
+      //     // 执行doLike方法
+      //     re(res)
+      //   })
+      // })
+      // // 判断执行失败时，还原列表状态
+      // if (rep.error) {
+      //   cardData.heart = !goalState
+      // }
+    }
   },
   watch: {},
   mounted() {
@@ -249,6 +283,7 @@ export default {
   border-width: 1px 0 1px 1px;
   border-radius: 16px 0 0 16px;
   border-style: solid;
+
   img {
     width: 24px;
   }
@@ -345,8 +380,22 @@ export default {
 .list-container {
   flex: 1;
   overflow: auto;
+  display: flex;
+  flex-direction: column;
+
   &::-webkit-scrollbar {
     width: 0;
+  }
+
+  .no-data {
+    img {
+      width: 420px;
+      margin-top: -10vh;
+    }
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 }
 
@@ -375,69 +424,27 @@ export default {
       cursor: pointer;
     }
 
-    &:hover>.card-shadow {
-      display: block;
+    &:hover .heart {
+      cursor: pointer;
+      opacity: 1;
     }
 
-    &:hover>.card-shadow>.card-shadow-btns {
-      display: flex;
-    }
-
-    .card-shadow {
-      display: none;
+    .heart {
       position: absolute;
-      top: 0;
-      bottom: 0;
-      width: 100%;
-      z-index: 2;
-      background-color: rgba(0, 0, 0, 0.8);
+      opacity: 0;
+      transition: opacity 0.3s;
+      width: 40px;
+      height: 40px;
+      display: flex;
+      background-color: white;
+      border-radius: 50%;
+      align-items: center;
+      justify-content: center;
+      right: 20px;
+      bottom: 20px;
 
-      .card-shadow-btns {
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        width: 100%;
-        display: none;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        gap: 5px;
-
-        >.el-button,
-        >.el-dropdown {
-          width: 80%;
-          margin: 0;
-        }
-
-        .reject-button {
-          width: 80%;
-          position: relative;
-
-          .el-button {
-            width: 100%;
-          }
-
-          .reject-select {
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            left: 0;
-            top: 0;
-            opacity: 0;
-            cursor: pointer;
-          }
-        }
-
-        >.el-dropdown>.el-button {
-          width: 100%;
-        }
-      }
-
-      .el-checkbox {
-        position: absolute;
-        z-index: 3;
-        top: 16px;
-        left: 16px;
+      img {
+        width: 19.5px;
       }
     }
   }
